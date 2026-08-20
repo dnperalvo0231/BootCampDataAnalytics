6e56f8b7bd9c65547a8e8fd74b5b44154b334ecb
dnperalvo0231 <daniperalvo14@gmail.com>
2026-08-19 22:17:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 import streamlit as st
+
+st.title("BootCamp Data Analytics for Oil & Gas")
+
+pozo = "SPE-001"
+oil_bpd=1250
+water_bpd=350.5
+status=True
+
+st.write(pozo)
+st.write(oil_bpd)
+st.write(water_bpd)
+st.write(status)
