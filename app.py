1706e5f5d90bebfb8417dcc5acc2ea898644fced
dnperalvo0231 <daniperalvo14@gmail.com>
2026-08-21 12:36:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,5 @@ elif modulos == "Funciones":
     """
     relacion_presion = presion_fondo/presion_yacimiento
     caudal = caudal_max*(1 - 0.2*relacion_presion - 0.8*(relacion_presion**2))
-    
-  return (round(caudal, decimales))
+    return (round(caudal, decimales))
   caudal_max = st.numbrer.imput("Ingrese el Caudal Maximo")
