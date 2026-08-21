f396b2f649425bcb7485b17881653fbc94d4ee65
dnperalvo0231 <daniperalvo14@gmail.com>
2026-08-21 12:35:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,20 @@ if modulos == "Introducción a Variables":
   st.write(status)
 elif modulos == "Funciones":
   def calcular_caudal_vogel(caudal_max=1000, presion_yacimiento=3000, presion_fondo=200, decimales=2):
-  """
-  Calcula el caudal de petroleo mediante la ecuacion de vogel
-
-  Parametros:
-  Caudal_maximo (float)
-  Presion de yacimiento(float)
-  presion_fondo (float)
-  Decimales (int)
-
-  Retorna:
-  caudal (float)
-  """
-  relacion_presion = presion_fondo/presion_yacimiento
-  caudal = caudal_max*(1 - 0.2*relacion_presion - 0.8*(relacion_presion**2))
+    """
+    Calcula el caudal de petroleo mediante la ecuacion de vogel
   
+    Parametros:
+    Caudal_maximo (float)
+    Presion de yacimiento(float)
+    presion_fondo (float)
+    Decimales (int)
+  
+    Retorna:
+    caudal (float)
+    """
+    relacion_presion = presion_fondo/presion_yacimiento
+    caudal = caudal_max*(1 - 0.2*relacion_presion - 0.8*(relacion_presion**2))
+    
   return (round(caudal, decimales))
   caudal_max = st.numbrer.imput("Ingrese el Caudal Maximo")
